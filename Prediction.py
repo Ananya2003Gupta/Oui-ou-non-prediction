31f9b5bba1148ec1ca7df760ce32bd39cff625c3
Ananya Gupta <90386813+Ananya2003Gupta@users.noreply.github.com>
2022-01-15 23:37:56 +0530

    Updated prediction.py

--- Prediction.py
+++ Prediction.py
@@ -1,5 +1,5 @@
 import random
 l=['Yes','No']
 r=l[random.randint(0,1)]
-n=input("Ask your dilemma to get an ans in yes or no:")
+n=input("Ask your question to know whether it is a 'yes' or 'no' :")
 print(r)
